dc09751e852abed88bef19c12cb5c752d474281d
Lucas Cipriano <lucas.cipra@gmail.com>
2023-10-21 19:44:08 -0300

    Initial commit

---
+++ src/test/java/lucas/soft/sistemavendas/Application.java
@@ -0,0 +1,13 @@
+package lucas.soft.sistemavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
