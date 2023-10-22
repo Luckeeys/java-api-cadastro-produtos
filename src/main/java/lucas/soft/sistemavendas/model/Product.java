8734e7a844bcc8c947f94757a258d05ed5fecd8f
Lucas Cipriano <lucas.cipra@gmail.com>
2023-10-21 21:01:10 -0300

    Implementando a camada de dominio

---
+++ src/main/java/lucas/soft/sistemavendas/model/Product.java
@@ -0,0 +1,87 @@
+package lucas.soft.sistemavendas.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Column(length = 300)
+    private String description;
+
+    @Column(scale = 10, precision = 2)
+    private BigDecimal price;
+
+    @Column(length = 5)
+    private Integer stock;
+
+    @Column(length = 30)
+    private String category;
+
+    @Column(length = 100)
+    private String  image_url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+}
